e9dd880cbeaa1b9f2b57f8873b7697999a1683ac
Dorftek <theviolator_89@hotmail.com>
2017-02-26 19:57:17 +0100

    small changes

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void loadLevel(string name){
+		Debug.Log("Loading "+name);
+		Application.LoadLevel (name);
+
+	}
+	public void exitProgram(){
+	
+		Application.Quit ();
+		Debug.Log ("shut down imminent ");
+
+	}
+
+}
